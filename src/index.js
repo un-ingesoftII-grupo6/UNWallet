adea2f150a5d13b90df1e64da601676fd7ea7109
Nicolás Rodríguez <nicrodriguezval@unal.edu.co>
2020-04-07 19:34:37 -0500

    Start to desing GUI and route to Log in and sign up

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 // setings 
 app.set('port', 8080)
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs');
 
 // middlewares 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index'/*, { title : 'UN Wallet'}*/);
-    //console.log(path.join(__dirname, 'views/index.html'));
+    res.render('index.html', { title : 'UN Wallet' });
 });
 
+router.get('/login', (req, res) => {
+    res.render('login.html', { title : 'Log in' });
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup.html', { title : 'Sign up' });
+});
+
+// Export module
 module.exports = router;
