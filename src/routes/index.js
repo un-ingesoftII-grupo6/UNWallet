1f4fd0dc2fefa496e61cad53666f3cd65ad24ad3
Nicolás Rodríguez <nicrodriguezval@unal.edu.co>
2020-04-10 20:55:07 -0500

    user sign up is stored in memory (for the moment), validation if user exist on memory for log in, and create wallet with it initial design

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var users = [];
 
 router.get('/', (req, res) => {
     res.render('index.html', { title : 'UN Wallet' });
@@ -13,10 +14,28 @@ router.get('/signup', (req, res) => {
     res.render('signup.html', { title : 'Sign up' });
 });
 
-router.post('/login', (req, res) => {
-    // Acá va la validación con la base de datos 
+router.get('/wallet', (req, res) => {
+    res.render('/wallet.html');
+});
 
-    res.send("Ok!");
+router.post('/login', (req, res) => {
+    // Acá va la validación con la base de datos
+    var flag = false;
+    var user = null;
+
+    for(var i = 0; i < users.length; i++) { //búsqueda de la existencia de un usuario (por el momento se trabaja en memoria)
+        if(req.body.email == users[i].email && req.body.password == users[i].password) {
+            user = users[i];
+            flag = true;
+            break;
+        }
+    }
+
+    if(flag) {
+        res.render('wallet.html', user);
+    } else {
+        res.send(400).send('El ususario no existe');
+    }  
 });
 
 router.post('/signup', (req, res) => {
@@ -24,7 +43,21 @@ router.post('/signup', (req, res) => {
     if(req.body.password != req.body.confirmPassword) {
         res.send(400).send("Las contraseñas no coinciden");
     } else {
-        res.send(req.body);
+        let wallet = { // Se debe agregar la wallet a la base de datos
+            balance : 0.0,
+            state : true
+        };
+
+        let user = { // El usuario debe ser guardado en la base de datos si todo está correcto
+            name : req.body.name,
+            lastName : req.body.lastName,
+            email : req.body.email,
+            password : req.body.password,
+            wallet_user : wallet
+        };
+
+        users.push(user);
+        res.render('wallet.html', user);
     }
 });
 
